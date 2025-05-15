bf1638ab2b13b6860e022a3d7c72a6139e2405e1
Timo Brogle <utnlg@student.kit.edu>
2025-05-15 18:50:35 +0200

    Parallelisierung + .svg output, anstelle von plt.show(). neuronale NEtze miteinander verglichen

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ from package1.Function_Chess import Function_Chess
 from package1.Approx_NN import NN_Approximator
 import numpy as np
 
-sinFunc = Sin2D_Function("Sinus(x*y)", (-2*(np.pi)),(2*(np.pi)),(-2*(np.pi)),(2*(np.pi)))
 function_chess = Function_Chess("Chess field",0,8,0,8)
-approx2 = Approx_Linear_Regression("Linear Model",[1000])
-approx3 = Identity_Approximator("Id3",[])
+function_sinChess = Sin2D_Function("sin(x)*cos(y)", (-2*(np.pi)),(2*(np.pi)),(-2*(np.pi)),(2*(np.pi)))
+
+approx_lin_1000 = Approx_Linear_Regression(f"Linear Model (1000 Samples)",[1000])
+approx_lin_500 = Approx_Linear_Regression(f"Linear Model (500 Samples)",[500])
+
+approx_id = Identity_Approximator("Id3",[])
+
+approx_nn_E500_L4_4_4 =NN_Approximator("name",[500,100,[4,4,4]])
+#sin-function experiment:
 approxList = []
-for i in range(7,8):
-    approxList.append(NN_Approximator("Neuronales Netz mit Layers: ",[3000,100,[i,i,i]]))
-exp = Experiment(approxList, sinFunc)
+for i in [4,8,12,16,32,64,128]:
+    for j in [1,2,4,8,16,32]:
+        name = f"Neuronales Netz E: {100*j} & L: {i} (fully connected)"
+        approxList.append(NN_Approximator(name,[10*j,100,[i,i,i]]))
+exp = Experiment(True, approxList, function_sinChess)
 exp.run()
--- package1/Experiment.py
+++ package1/Experiment.py
@@ -1,50 +1,99 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+
+import time
+
+def train_and_predict(approximator_data):
+    import copy
+    approximator, function_class, function_params, X, Y, Z_true = approximator_data
+
+    # Deepcopy (Sicherheitsmaßnahme)
+    approximator = copy.deepcopy(approximator)
+    function = function_class(*function_params)
+
+    start_train = time.time()
+    approximator.train(function)
+    train_time = time.time() - start_train
+
+    start_eval = time.time()
+    Z_pred = approximator.predict(X, Y)
+    eval_time = time.time() - start_eval
+
+    mse = np.mean((Z_true - Z_pred) ** 2)
+    return approximator.name, Z_pred, mse, train_time, eval_time
+ 
+
+from concurrent.futures import ProcessPoolExecutor
+import copy
 
 class Experiment:
-    def __init__(self, approximators, function):
+    def __init__(self, parallel, approximators, function):
         self.approximators = approximators
         self.function = function
+        self.parallel = parallel
 
     def run(self):
         x_fine = np.linspace(self.function.xdomainstart, self.function.xdomainend, 100)
         y_fine = np.linspace(self.function.ydomainstart, self.function.ydomainend, 100)
         X, Y = np.meshgrid(x_fine, y_fine)
-
         Z_true = self.function.evaluate(X, Y)
 
         n_approx = len(self.approximators)
-        
-        # Erstelle eine Figur mit 1 Reihe, n_approx Spalten
-        fig, axes = plt.subplots(1, n_approx+1, figsize=(5*(n_approx+1), 4))
-        
-        # Erster Plot: Originalfunktion
+
+        max_cols = 3
+        total_plots = n_approx + 1
+        n_cols = min(max_cols, total_plots)
+        n_rows = (total_plots + n_cols - 1) // n_cols
+        fig, axes = plt.subplots(n_rows, n_cols, figsize=(5 * n_cols, 4 * n_rows))
+        axes = np.array(axes).reshape(-1)
+
         ax = axes[0]
         im = ax.imshow(Z_true, extent=[self.function.xdomainstart, self.function.xdomainend,
-                                      self.function.ydomainstart, self.function.ydomainend],
-                       origin='lower', cmap='plasma', aspect='auto')
+                                       self.function.ydomainstart, self.function.ydomainend],
+                       origin='lower', cmap='viridis', aspect='auto')
         ax.set_title("Original Function")
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 
-        # Plotte jede Approximation in eigenen Subplots
-        for i, approximator in enumerate(self.approximators):
-            approximator.train(self.function)
-            Z_pred = approximator.predict(X, Y)
+        if not self.parallel:
+            results = []
+            for approximator in self.approximators:
+                approximator.train(self.function)
+                Z_pred = approximator.predict(X, Y)
+                mse = np.mean((Z_true - Z_pred) ** 2)
+                results.append((approximator.name, Z_pred, mse, None, None))
+        else:
+            function_class = self.function.__class__
+            function_params = self.function.__dict__.values()
 
-            mse = np.mean((Z_true - Z_pred) ** 2)
-            
-            ax = axes[i+1]
+            data = [
+                (copy.deepcopy(approximator), function_class, list(function_params),
+                 X, Y, Z_true)
+                for approximator in self.approximators
+            ]
+            with ProcessPoolExecutor() as executor:
+                results = list(executor.map(train_and_predict, data))
+
+        for i, (name, Z_pred, mse, train_time, eval_time) in enumerate(results):
+            ax = axes[i + 1]
             im = ax.imshow(Z_pred, extent=[self.function.xdomainstart, self.function.xdomainend,
-                                          self.function.ydomainstart, self.function.ydomainend],
+                                           self.function.ydomainstart, self.function.ydomainend],
                            origin='lower', cmap='viridis', aspect='auto')
-            ax.set_title(f'Approximator {i+1} (MSE: {mse:.4f})')
+            title = f'{name}\n (MSE: {mse:.4f})'
+            if train_time is not None:
+                title += f'\nTrain: {train_time:.2f}s, Eval: {eval_time:.2f}s'
+            ax.set_title(title)
             ax.set_xlabel('x')
             ax.set_ylabel('y')
             fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 
-        plt.tight_layout()
-        plt.show()
+        for axis in axes[total_plots:]:
+            axis.axis('off')
 
-        return [approximator.predict(X, Y) for approximator in self.approximators]
+        plt.tight_layout()
+        plt.savefig("results_large.svg", format="svg")
+        if np.size(self.approximators)<4:
+            plt.show()
+        return [r[1] for r in results]
