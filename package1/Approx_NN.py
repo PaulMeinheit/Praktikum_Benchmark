Small fixes with logarithmic scaling

package1/Approx_NN.py
@@ -15,15 +15,28 @@ class NN_Approximator(Approximator):
         self.nodesPerLayer = params[2]
         self.nn_general = NN_General(self.epochs,self.samplePoints,self.nodesPerLayer)
 
-    def generate_data(self,function,samplePoints):
+    def generate_linear_data(self,function,samplePoints):
         x = np.linspace(function.xdomainstart, function.xdomainend, samplePoints)
         y = np.linspace(function.ydomainstart, function.ydomainend, samplePoints)
         X, Y = np.meshgrid(x, y)
         Z = function.evaluate(X,Y)
         return X, Y, Z
 
+    def generate_linear_data(self,function,samplePoints):
+        x = np.linspace(function.xdomainstart, function.xdomainend, samplePoints)
+        y = np.linspace(function.ydomainstart, function.ydomainend, samplePoints)
+        X, Y = np.meshgrid(x, y)
+        Z = function.evaluate(X,Y)
+        return X, Y, Z
+
+    def generate_random_data(self,function,samplePoints):
+        X=None
+        Y=None
+        Z=None
+        return X,Y,Z
+
     def train(self,function):
-        X, Y,Z = self.generate_data(function,self.samplePoints)
+        X, Y,Z = self.generate_linear_data(function,self.samplePoints)
 
         inputs = np.stack([X.ravel(), Y.ravel()], axis=1)
         targets = Z.ravel().reshape(-1, 1)

package1/Experiment.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
-
+from matplotlib.colors import LogNorm 
 import time
 
 def train_and_predict(approximator_data):
@@ -16,9 +16,10 @@ def train_and_predict(approximator_data):
     approximator.train(function)
     train_time = time.time() - start_train
 
-    start_eval = time.time()
+    start_eval = time.process_time_ns
+    
     Z_pred = approximator.predict(X, Y)
-    eval_time = time.time() - start_eval
+    eval_time = time.process_time_ns - start_eval
 
     mse = np.mean((Z_true - Z_pred) ** 2)
     return approximator.name, Z_pred, mse, train_time, eval_time
@@ -28,10 +29,14 @@ from concurrent.futures import ProcessPoolExecutor
 import copy
 
 class Experiment:
-    def __init__(self, parallel, approximators, function):
+    def __init__(self, parallel,logarithmic, approximators, function, vmin=None, vmax=None):
         self.approximators = approximators
         self.function = function
         self.parallel = parallel
+        self.logarithmic=logarithmic
+        self.vmin = vmin
+        self.vmax = vmax
+        
 
     def run(self):
         x_fine = np.linspace(self.function.xdomainstart, self.function.xdomainend, 100)
@@ -47,11 +52,15 @@ class Experiment:
         n_rows = (total_plots + n_cols - 1) // n_cols
         fig, axes = plt.subplots(n_rows, n_cols, figsize=(5 * n_cols, 4 * n_rows))
         axes = np.array(axes).reshape(-1)
-
         ax = axes[0]
+         
+        norm = None
+        if self.logarithmic:
+            norm = LogNorm(vmin=self.vmin, vmax=self.vmax)
+            Z_true = np.clip(Z_true,self.vmin,self.vmax)
         im = ax.imshow(Z_true, extent=[self.function.xdomainstart, self.function.xdomainend,
                                        self.function.ydomainstart, self.function.ydomainend],
-                       origin='lower', cmap='viridis', aspect='auto')
+                       origin='lower', cmap='viridis', aspect='auto', norm=norm)
         ax.set_title("Original Function")
         ax.set_xlabel('x')
         ax.set_ylabel('y')
@@ -75,12 +84,13 @@ class Experiment:
             ]
             with ProcessPoolExecutor() as executor:
                 results = list(executor.map(train_and_predict, data))
-
         for i, (name, Z_pred, mse, train_time, eval_time) in enumerate(results):
             ax = axes[i + 1]
+            if self.logarithmic:
+                Z_pred = np.clip(Z_pred,self.vmin,self.vmax)
             im = ax.imshow(Z_pred, extent=[self.function.xdomainstart, self.function.xdomainend,
                                            self.function.ydomainstart, self.function.ydomainend],
-                           origin='lower', cmap='viridis', aspect='auto')
+                           origin='lower', cmap='viridis', aspect='auto', norm=norm)
             title = f'{name}\n (MSE: {mse:.4f})'
             if train_time is not None:
                 title += f'\nTrain: {train_time:.2f}s, Eval: {eval_time:.2f}s'