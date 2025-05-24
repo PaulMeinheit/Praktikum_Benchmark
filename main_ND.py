690e7620ee12a6017d642f0a3793dab4e2b0f84f
Timo Brogle <utnlg@student.kit.edu>
2025-05-24 14:17:40 +0200

    bro was es geht einfach

--- main_ND.py
+++ main_ND.py
@@ -3,10 +3,16 @@ from multiDim.Approximator_NN_ND import Approximator_NN_ND
 from multiDim.Function_SinChess_4D import Function_Sin_4D
 from multiDim.Function_SinChess_2D import Function_Sin_2D
 from multiDim.Function_multiDimOutput import Function_MultiDimOutput
+from multiDim.ShepardInterpolator import ShepardInterpolator
 import numpy as np
 
 
-exp = Experiment_ND("Test-Experiment",[Approximator_NN_ND("Versuch NN 4-4-1",[7000,400,[64,64,64,64]])],Function_MultiDimOutput())
+def getApprox():
+    approx_nn_nd =Approximator_NN_ND("Versuch NN 64-64-64-64 & 7k Epochen & 400^n Punkten",[1000,100,[64,64,64,64]])
+    shepard = ShepardInterpolator([],100)
+    apprx = [shepard, approx_nn_nd]
+    return apprx
+exp = Experiment_ND("Test-Experiment",getApprox(),Function_MultiDimOutput())
 exp.train()
 exp.print_loss_summary()
 exp.plot_1d_slices()
--- multiDim/ShepardInterpolator.py
+++ multiDim/ShepardInterpolator.py
@@ -1,39 +1,39 @@
-import ApproximatorND
-import FunctionND
 import numpy as np
-class ShepardInterpolator (ApproximatorND):
+from multiDim.ApproximatorND import ApproximatorND
+class ShepardInterpolator(ApproximatorND):
     
-    def __init__(self, name,params,power, numPoints):
+    def __init__(self, params, numPoints,power=2,name = "ShepardInterpolator_multi_dim"):
         self.name = name
         self.params = params
-        self.inputDim = 0
-        self.outputDim = 0
         self.power = power
         self.eps = 1e-12
         self.numPoints = numPoints
+        self.inputDim = 0
+        self.outputDim = 0
 
     def train(self, function):
-        inputDim = function.inputDim
-        outputDim = function.outputDim
-        self.dataPoints = np.random.uniform(function.inDomainStart,function.inDomainEnd, size=(self.numPoints, inputDim))
+        self.inputDim = function.inputDim
+        self.outputDim = function.outputDim
+        self.dataPoints = np.random.uniform(function.inDomainStart, function.inDomainEnd, size=(self.numPoints, self.inputDim))
         values = function.evaluate(self.dataPoints)
-        
-        self.values = np.asarray(values)
-        
-    def predict(self, input):
+        self.values = np.asarray(values)  # sollte shape (numPoints, outputDim) haben
 
-        query_points = np.asarray(input)
-        interpolated_values = np.empty(len(query_points))
+    def predict(self, input):
+        query_points = np.asarray(input)  # shape (numQueryPoints, inputDim)
+        numQueryPoints = query_points.shape[0]
+        interpolated_values = np.empty((numQueryPoints, self.outputDim))
 
         for i, qp in enumerate(query_points):
-            dists = np.linalg.norm(self.data_points - qp, axis=1)
+            dists = np.linalg.norm(self.dataPoints - qp, axis=1)  # Distanz zu allen Trainingspunkten
             weights = 1.0 / (dists**self.power + self.eps)
 
+            # Falls ein Punkt genau auf einem Trainingspunkt liegt, gib dessen Wert direkt zurück
             if np.any(dists < self.eps):
-                interpolated_values[i] = self.values[np.argmin(dists)]
+                interpolated_values[i, :] = self.values[np.argmin(dists)]
             else:
-                weighted_sum = np.sum(weights * self.values)
+                # Gewichtete Summe für jede Output-Dimension separat
+                weighted_sum = np.sum(weights[:, np.newaxis] * self.values, axis=0)
                 sum_weights = np.sum(weights)
-                interpolated_values[i] = weighted_sum / sum_weights
+                interpolated_values[i, :] = weighted_sum / sum_weights
 
         return interpolated_values
