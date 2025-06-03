f81f7436552cdaea971de2070675d735afe1fd1a
Paul Meinheit <ufogg@student.kit.edu>
2025-06-03 14:25:55 +0200

    arm update

--- multiDim/BasicArm.py
+++ multiDim/BasicArm.py
@@ -14,10 +14,9 @@ class BasicArm(FunctionND):
         self.my_chain = Chain(name='3-link-arm', links=[
             URDFLink(name="base", translation=[0, 0, 0], rotation=[0, 0, 0]),
             URDFLink(name="joint1", translation=[1, 0, 0], rotation=[0, 0, 0], bounds=(-3.14, 3.14)),
-            URDFLink(name="joint2", translation=[1, 0, 0], rotation=[0, 0, 0], bounds=(-3.14, 3.14)),
-])
+            URDFLink(name="joint2", translation=[1, 0, 0], rotation=[0, 0, 0], bounds=(-3.14, 3.14)),])
 
-        def evaluate (self,input):
+    def evaluate (self,input):
             output = np.array()
             for i in range(1,input.shape[1]):
                 results = self.my_chain.forward_kinematics(input[i])
